148182dc3e38ab8cba898d055df573bcde8c28be
MiKPo4eLiK <stassojcuk@gmail.com>
2025-06-01 15:21:17 +0300

    well

--- app/main.py
+++ app/main.py
@@ -2,38 +2,46 @@ import math
 
 
 class Vector:
-    def __init__(self, x: float, y: float) -> None:
-        self.x = round(x, 2)
-        self.y = round(y, 2)
+    def __init__(self, coordinate_x: float, coordinate_y: float) -> None:
+        self.coordinate_x = round(coordinate_x, 2)
+        self.coordinate_y = round(coordinate_y, 2)
 
     def __add__(self, other: "Vector") -> "Vector":
-        return Vector(self.x + other.x, self.y + other.y)
+        if not isinstance(other, Vector):
+            raise TypeError("Unsupported addition type.")
+        return Vector(self.coordinate_x + other.coordinate_x,
+                      self.coordinate_y + other.coordinate_y)
 
     def __sub__(self, other: "Vector") -> "Vector":
-        return Vector(self.x - other.x, self.y - other.y)
+        if not isinstance(other, Vector):
+            raise TypeError("Unsupported subtraction type.")
+        return Vector(self.coordinate_x - other.coordinate_x,
+                      self.coordinate_y - other.coordinate_y)
 
-    def __mul__(self, other) -> "Vector | float":
+    def __mul__(self, other: float | "Vector") -> "Vector | float":
         if isinstance(other, (int, float)):
-            return Vector(self.x * other, self.y * other)
-        elif isinstance(other, Vector):
-            return round(self.x * other.x + self.y * other.y, 4)
+            return Vector(self.coordinate_x * other, self.coordinate_y * other)
+        if isinstance(other, Vector):
+            return round(self.coordinate_x * other.coordinate_x
+                         + self.coordinate_y * other.coordinate_y, 4)
         raise TypeError("Unsupported multiplication type.")
 
     @classmethod
-    def create_vector_by_two_points(
-            cls, start_point: tuple[float, float],
-            end_point: tuple[float, float]) -> "Vector":
-        return cls(end_point[0] - start_point[0], end_point[1] -
-                   start_point[1])
+    def create_vector_by_two_points(cls,
+                                    start_point: tuple[float, float],
+                                    end_point: tuple[float, float]) \
+            -> "Vector":
+        return cls(end_point[0] - start_point[0],
+                   end_point[1] - start_point[1])
 
     def get_length(self) -> float:
-        return math.sqrt(self.x ** 2 + self.y ** 2)
+        return math.sqrt(self.coordinate_x ** 2 + self.coordinate_y ** 2)
 
     def get_normalized(self) -> "Vector":
         length = self.get_length()
         if length == 0:
-            return Vector(0, 0)
-        return Vector(self.x / length, self.y / length)
+            return Vector(0.0, 0.0)
+        return Vector(self.coordinate_x / length, self.coordinate_y / length)
 
     def angle_between(self, other: "Vector") -> int:
         dot_product = self * other
@@ -41,11 +49,13 @@ class Vector:
         return round(math.degrees(math.acos(cos_angle)))
 
     def get_angle(self) -> int:
-        cos_angle = self.y / self.get_length()
+        cos_angle = self.coordinate_y / self.get_length()
         return round(math.degrees(math.acos(cos_angle)))
 
     def rotate(self, degrees: int) -> "Vector":
         radians = math.radians(degrees)
-        new_x = self.x * math.cos(radians) - self.y * math.sin(radians)
-        new_y = self.x * math.sin(radians) + self.y * math.cos(radians)
+        new_x = (self.coordinate_x * math.cos(radians)
+                 - self.coordinate_y * math.sin(radians))
+        new_y = (self.coordinate_x * math.sin(radians)
+                 + self.coordinate_y * math.cos(radians))
         return Vector(new_x, new_y)
