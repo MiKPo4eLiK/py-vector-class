f2a71f7475a3e4672d3002baa48ae487441b7465
MiKPo4eLiK <stassojcuk@gmail.com>
2025-06-01 15:15:57 +0300

    ghghghhghg

--- app/main.py
+++ app/main.py
@@ -1,3 +1,51 @@
+import math
+
+
 class Vector:
-    # write your code here
-    pass
+    def __init__(self, x: float, y: float) -> None:
+        self.x = round(x, 2)
+        self.y = round(y, 2)
+
+    def __add__(self, other: "Vector") -> "Vector":
+        return Vector(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other: "Vector") -> "Vector":
+        return Vector(self.x - other.x, self.y - other.y)
+
+    def __mul__(self, other) -> "Vector | float":
+        if isinstance(other, (int, float)):
+            return Vector(self.x * other, self.y * other)
+        elif isinstance(other, Vector):
+            return round(self.x * other.x + self.y * other.y, 4)
+        raise TypeError("Unsupported multiplication type.")
+
+    @classmethod
+    def create_vector_by_two_points(
+            cls, start_point: tuple[float, float],
+            end_point: tuple[float, float]) -> "Vector":
+        return cls(end_point[0] - start_point[0], end_point[1] -
+                   start_point[1])
+
+    def get_length(self) -> float:
+        return math.sqrt(self.x ** 2 + self.y ** 2)
+
+    def get_normalized(self) -> "Vector":
+        length = self.get_length()
+        if length == 0:
+            return Vector(0, 0)
+        return Vector(self.x / length, self.y / length)
+
+    def angle_between(self, other: "Vector") -> int:
+        dot_product = self * other
+        cos_angle = dot_product / (self.get_length() * other.get_length())
+        return round(math.degrees(math.acos(cos_angle)))
+
+    def get_angle(self) -> int:
+        cos_angle = self.y / self.get_length()
+        return round(math.degrees(math.acos(cos_angle)))
+
+    def rotate(self, degrees: int) -> "Vector":
+        radians = math.radians(degrees)
+        new_x = self.x * math.cos(radians) - self.y * math.sin(radians)
+        new_y = self.x * math.sin(radians) + self.y * math.cos(radians)
+        return Vector(new_x, new_y)
